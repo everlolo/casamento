f43b86c5ac811ed42d81465baf272d7c37f802e4
everlolo <everlolo@gmail.com>
2025-11-13 21:22:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -363,7 +363,7 @@ if (checkNomeBtn) {
    RSVP VIA PIN — FRONT-END
    ============================================================ */
 
-const URL_WEBAPP = "COLE_AQUI_SUA_URL_DO_WEBAPP_PUBLICADO";
+const URL_WEBAPP = "https://script.google.com/macros/s/AKfycbygYup61ahqKlAPN5Nr0_ldLItzN3MwFUU1GQl0-b6K-6J5-MDUr_bbCWz33NlAMgmvoA/exec";
 
 const pinInput = document.getElementById("pinInput");
 const buscarBtn = document.getElementById("buscarPinBtn");
@@ -449,6 +449,7 @@ salvarBtn.addEventListener("click", async () => {
     mensagem.textContent = "Erro ao salvar.";
   }
 });
+
 
 
 
